5480a7fd72de20097579db390285f37728371717
Katerina Maria Gerakianaki <77936483+categerjsy@users.noreply.github.com>
2024-11-26 13:43:54 +0200

    container-header

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from '../components/category/category.component';
 import { IonicModule } from '@ionic/angular';
+import { CategorySearchComponent } from '../components/category-search/category-search.component';
 
 
 @NgModule({
-  declarations: [CategoryComponent],
-  exports: [CategoryComponent],
+  declarations: [CategoryComponent, CategorySearchComponent],
+  exports: [CategoryComponent, CategorySearchComponent],
   imports: [
     CommonModule,
     IonicModule
